a53fa22129cc8ba4887b86714fa914060b164e25
Kevin Vizcarra <kevinvizcarra11@gmail.com>
2017-08-26 19:11:38 -0700

    Use Room and LiveData for `NoteListViewModel`

--- app/src/main/java/com/kevin/notes/NoteListViewModel.java
+++ app/src/main/java/com/kevin/notes/NoteListViewModel.java
@@ -2,30 +2,21 @@ package com.kevin.notes;
 
 
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import com.kevin.notes.db.Note;
+
 import java.util.List;
 
 class NoteListViewModel extends ViewModel {
 
-    private MutableLiveData<List<Note>> notes;
+    private LiveData<List<Note>> notes;
 
-    public LiveData<List<Note>> getNotes() {
-        if (notes == null) {
-            notes = new MutableLiveData<>();
-            loadNotes();
-        }
-        return notes;
+    public NoteListViewModel() {
+        notes = NoteApplication.getDb().noteDao().loadAllNotes();
     }
 
-    private void loadNotes() {
-        notes.setValue(new ArrayList<>(Arrays.asList(
-                new Note("Title 1", "Value 1"),
-                new Note("Title 2", "Value 2"),
-                new Note("Title 3", "Value 3")
-        )));
+    public LiveData<List<Note>> getNotes() {
+        return notes;
     }
 }
